fdd312060cf0d368ae6a04402964be292ed2b36b
dugiheal <133659469+dugiheal@users.noreply.github.com>
2023-06-16 15:10:39 +0300

    #

--- main.cpp
+++ main.cpp
@@ -5,5 +5,48 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "ru");
-    cout << "Hello DugiHeal" << endl;
+
+    int counter = 0;
+    int leng;
+    char symb;
+    int orientathon;
+
+    cout << "Input symbol  -> ";
+    cin >> symb;
+    cout << "Input lengths -> ";
+    cin >> leng;
+    cout << "Input orientathion:" << endl
+         << "\t1. gorizont " << endl
+         << "\t2. vertical " << endl
+         << "\t->";
+    cin >> orientathon;
+
+    switch (orientathon)
+    {
+    case 1:
+    {
+        while (counter != leng)
+        {
+            cout << symb;
+            counter += 1;
+        }
+        break;
+    }
+
+    case 2:
+    {
+        while (counter != leng)
+        {
+            cout << symb << endl;
+            counter += 1;
+        }
+        break;
+    }
+
+    default:
+    {
+        cout << "Error!";
+        break;
+    }
+    }
 }
