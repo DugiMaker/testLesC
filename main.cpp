692ceb29105f939d4aeae3e56d2bfe12fe802f8f
dugiheal <133659469+dugiheal@users.noreply.github.com>
2023-06-16 12:56:38 +0300

    #

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// int main()
+// {
+//     //calc
+//     setlocale(LC_ALL, "ru");
+//     int a, b, c;
+//     string d;
+
+//     cout << "2 nubm insert:\n->";
+//     cin >> a;
+//     cout << "->";
+//     cin >> b;
+
+//     cout << "Choose operator:" << endl
+//          << "1. +" << endl
+//          << "2. -" << endl
+//          << "3. *" << endl
+//          << "4. /" << endl
+//          << "5. %" << endl
+//          << "->";
+//     cin >> c;
+//     // menu calc
+//     switch (c)
+//     {
+//     case 1:
+//     {
+//         cout << a << "+" << b << "=" << a + b;
+//         break;
+//     }
+//     case 2:
+//     {
+//         cout << a << " - " << b << " = " << a - b;
+//         break;
+//     }
+//     case 3:
+//     {
+//         cout << a << " * " << b << " = " << a * b;
+//         break;
+//     }
+//     case 4:
+//     {
+//         cout << a << " / " << b << " = " << a / b;
+//         break;
+//     }
+//     case 5:
+//     {
+//         c = a % b;
+//         cout << a << " % " << b << " = " << c << endl;
+//         if (c == 0)
+//         {
+//             cout << "True!";
+//         }
+//         else
+//         {
+//             cout << "False";
+//         }
+//         break;
+//     }
+//     default:
+//     {
+//         cout << "Error!";
+//         break;
+//     }
+//     }
+// }
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+}
